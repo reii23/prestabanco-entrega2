0ebf64032a71d1a4d39aab8c33173f5c77871334
reii23 <reinaldo.pacheco@usach.cl>
2024-11-28 11:28:53 -0300

    add repository in ms-simulation repository
    se agrega el @Repository para ms-simulation en el repositorio

--- ms-simulation/src/main/java/com/prestabanco/mssimulation/repositories/CreditSimulationRepository.java
+++ ms-simulation/src/main/java/com/prestabanco/mssimulation/repositories/CreditSimulationRepository.java
@@ -2,6 +2,8 @@ package com.prestabanco.mssimulation.repositories;
 
 import com.prestabanco.mssimulation.entities.CreditSimulationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CreditSimulationRepository extends JpaRepository<CreditSimulationEntity, Long> {
 }
