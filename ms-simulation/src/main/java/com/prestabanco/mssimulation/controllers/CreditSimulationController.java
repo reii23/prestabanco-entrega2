239813a3852b9e199c08d81a32c5943e9ee61e66
reii23 <reinaldo.pacheco@usach.cl>
2024-11-30 01:03:30 -0300

    change request param to request body in simulation
    se cambia la forma de recibir los parametros en el ms-simulation ya que se eliminó entity y repository

--- ms-simulation/src/main/java/com/prestabanco/mssimulation/controllers/CreditSimulationController.java
+++ ms-simulation/src/main/java/com/prestabanco/mssimulation/controllers/CreditSimulationController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/simulation")
 public class CreditSimulationController {
@@ -13,11 +15,11 @@ public class CreditSimulationController {
     private CreditSimulationService creditSimulationService;
 
     @PostMapping("/")
-    public ResponseEntity<Double> calculateMonthlyFee(
-            @RequestParam double loanAmount,
-            @RequestParam double interestRate,
-            @RequestParam int termYears
-    ) {
+    public ResponseEntity<Double> calculateMonthlyFee(@RequestBody Map<String, Object> requestData) {
+        double loanAmount = Double.parseDouble(requestData.get("loanAmount").toString());
+        double interestRate = Double.parseDouble(requestData.get("interestRate").toString());
+        int termYears = Integer.parseInt(requestData.get("termYears").toString());
+
         double monthlyFee = creditSimulationService.calculateMonthlyFee(loanAmount, interestRate, termYears);
         return ResponseEntity.ok(monthlyFee);
     }
