4bdf065bd30793355be8fca42182be21b31b8347
reii23 <reinaldo.pacheco@usach.cl>
2024-12-04 14:38:53 -0300

    update url  services in front
    se actualizan las url para conectarse correctamente a los controladores del backend

--- front/interface/src/services/client.service.js
+++ front/interface/src/services/client.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://172.23.4.59:32216/api/v1/clients/";
+const API_URL = "http://172.23.4.59:32216/api/v1/users/";
 
 // obtain all clients
 const getAllClients = () => {
