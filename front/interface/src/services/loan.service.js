8a9e64a8917161e04387dd96633299f7cb11ce88
reii23 <reinaldo.pacheco@usach.cl>
2024-12-08 03:03:33 -0300

    update ports in services front
    se actualizan los puertos para conectar con el gateway

--- front/interface/src/services/loan.service.js
+++ front/interface/src/services/loan.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://172.23.4.59:32216/api/v1/request/";
+const API_URL = "http://172.23.4.59:30474/api/v1/request/";
 
 // obtain all loans from the API
 const getAllLoans = () => {
